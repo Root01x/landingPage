47131c1c17a8ce384e3d95e58549c6cf0c72bc68
Dave Rodriguez <root01x@gmail.com>
2023-02-08 21:24:52 -0500

    add counter and responsive mobile

--- counter.js
+++ counter.js
@@ -1,1 +1,14 @@
+const $ = (id) => document.querySelector(id)
+let valueCount = 1
 
+$('.btnMinus').addEventListener('click',
+  () => {
+    valueCount = valueCount - 1
+    valueCount < 1 ? valueCount = 1 : $('#count').value = valueCount
+  })
+
+$('.btnPlus').addEventListener('click',
+  () => {
+    valueCount = valueCount + 1
+    valueCount > 100 ? valueCount = 100 : $('#count').value = valueCount
+  })
